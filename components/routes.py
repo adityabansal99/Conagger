9bb80602b81a0a37d03ecbae4565681b3b088ba9
adityabansal99 <adityabnr73@gmail.com>
2020-12-19 12:49:50 +0530

    Minor refactoring

--- components/routes.py
+++ components/routes.py
@@ -2,12 +2,12 @@ import aiohttp
 import asyncio
 from components import app, source
 from components import source
-from flask import request
+from flask import render_template, request
 
-QUANTA_MAGAZINE = 'quantamagazine'
-REUTERS = 'reuters'
-TECH_CRUNCH = 'techcrunch'
-WIRED = 'wired'
+QUANTA_MAGAZINE = 'QuantaMagazine'
+REUTERS = 'Reuters'
+TECH_CRUNCH = 'TechCrunch'
+WIRED = 'Wired'
 
 source_uri = {
     QUANTA_MAGAZINE  : 'https://www.quantamagazine.org/',
@@ -15,22 +15,6 @@ source_uri = {
     TECH_CRUNCH      : 'https://www.techcrunch.com/',
     WIRED            : 'https://www.wired.com/',
 }
-
-
-async def fetch_source_response(session,url):
-    async with session.get(url,headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0'}) as response:
-        body = (await response.text())
-        return body
-
-
-async def fetch_sources_response(content_sources):
-    responses = []
-    async with aiohttp.ClientSession() as session:
-        for content_source in content_sources:
-            resp_body = asyncio.ensure_future(fetch_source_response(session,content_source.uri))
-            responses.append(resp_body)
-            content_source.response = resp_body
-        await asyncio.gather(*responses, return_exceptions=True)
 
 
 def make_sources(content_sources):
@@ -43,6 +27,26 @@ def make_sources(content_sources):
         sources.append(source.TechCrunch(source_uri[TECH_CRUNCH]))
     if WIRED in content_sources:
         sources.append(source.Wired(source_uri[WIRED]))
+    return sources
+
+
+async def fetch_source(session,url):
+    async with session.get(url,headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0'}) as response:
+        body = (await response.text())
+        return body
+
+
+async def fetch_sources(content_sources):
+    responses = []
+    sources = make_sources(content_sources)
+    async with aiohttp.ClientSession() as session:
+        for source in sources:
+            resp_body = asyncio.ensure_future(fetch_source(session,source.uri))
+            responses.append(resp_body)
+            source.response = resp_body
+        await asyncio.gather(*responses, return_exceptions=True)
+    responses = None
+    return sources
 
 
 @app.route('/')
@@ -53,13 +57,12 @@ def index():
 @app.route('/home',methods=['POST'])
 def home():
     content_sources = request.form.get('content_sources').split(',')
-    sources = make_sources(content_sources)
 
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    loop.run_until_complete(fetch_sources_response(sources))
+    sources = loop.run_until_complete(fetch_sources(content_sources))
 
     
-    return "Completed"
+    return render_template('base.htm',sources=sources)
 
 
